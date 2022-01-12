484511300bcfb76db734c77185cf475a14507c7b
Joe <hutcheson2001@yahoo.com>
2022-01-12 17:49:17 -0500

    commit tree 9486de10ee82e7615ba31ef40a850fc69211587a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const saturdayFun = function(activity="roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+  }
+  
+saturdayFun();
+
+const mondayWork = function(activity2="go to the office"){
+    return (`This Monday, I will ${activity2}.`);
+}
+
+mondayWork();
+
+const wrapAdjective = function(frontFlair="*"){
+    return function(complimentAdj="special"){
+        return `You are ${frontFlair}${complimentAdj}${frontFlair}!`;
+    }
+}
+
+wrapAdjective()();
